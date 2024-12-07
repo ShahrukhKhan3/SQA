bf7095e57ee8a0aee66efc041c7ea9751972ebda
ShahrukhKhan3 <104714695+ShahrukhKhan3@users.noreply.github.com>
2024-12-07 15:56:00 +0500

    first commit

---
+++ src/main/java/SQA/SQASELENIUM/App.java
@@ -0,0 +1,29 @@
+package SQA.SQASELENIUM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ */
+public class App {
+	@Test
+	 public void Test1() {
+	        System.out.println("Test1");
+	    }
+	@Test
+	 public void Test2() {
+	        System.out.println("Test2");
+	    }
+	 public void Test3() {
+	        System.out.println("Test3");
+	    }
+    public static void main(String[] args) {
+    	
+}
+   
+    }
+
